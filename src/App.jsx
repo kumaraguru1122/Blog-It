122e2884d956a80bbc74e245b4f3cb32876c5efb
dev-kumaraguru <dev.kumaraguru@gmail.com>
2025-05-25 22:17:33 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-7xl text-blue-700 text-center grid place-content-center min-h-screen font-bold">
+      BlogIt
+    </div>
+  );
+};
+
+export default App;
